ec14db574fb2689100b31388692d16dd3fe129af
c00ldy <mevadadhruval7529@gmail.com>
2024-03-21 23:02:30 +0530

    conversionOperation

---
+++ 01basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = 22;
+// console.log(typeof(score));
+// console.log(typeof score );
+
+let anew = "22";
+// console.log(typeof(anew));
+
+// ***************** operation ********
+
+let value = 3 
+let negvalue = -value
+// console.log(negvalue);
+
+
+let a = "dhruval"
+let b = " mevada"
+let c = a+b;
+// console.log(c);
+
+
+console.log(+true);
+console.log(+"");

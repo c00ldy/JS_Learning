8e632ac1e41c47afc04ab66f4a83cb67be1f8954
c00ldy <mevadadhruval7529@gmail.com>
2024-03-24 22:56:23 +0530

    number and maths

---
+++ 01basics/06_numandmath.js
@@ -0,0 +1,36 @@
+const score = 400;
+// console.log(score);
+
+const a =  new Number(50);
+// console.log(a);
+
+// console.log(a.toString());
+// console.log(a.toFixed(2));
+// console.log(a.toFixed(1));
+
+let b = 24.4903;
+// console.log(b.toFixed(2));
+// console.log(b.toPresicion(2));
+
+const hundreds = 100000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ************************math***************************
+
+// console.log(Math);
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.sqrt(5));
+// console.log(Math.min(4,5,9,1,0));
+// console.log(Math.max(4,5,9,1,0));
+ 
+// console.log(Math.random());
+
+let max = 6;
+let min = 1;
+
+
+// ------------------this is an important formula -----------------
+
+console.log(Math.floor (Math.random() * (max - min + 1 ) ) + min);

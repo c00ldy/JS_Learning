2a8c3d6ff0729ecace41c2b4a9f6c2fdbd00199f
c00ldy <mevadadhruval7529@gmail.com>
2024-03-26 22:02:04 +0530

    dates

---
+++ 01basics/07_dates.js
@@ -0,0 +1,18 @@
+// let mydate = new Date();
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(typeof mydate);
+
+// let creatdate = new Date(2023 , 0 , 23 , 5 , 3 )
+// let creatdate = new Date("01-14-2023");
+// console.log(creatdate.toLocaleString());
+
+// let mytime = date.now();
+// console.log(creatdate.getTime());
+
+let newdata = new Date();
+console.log(newdata);
+console.log(newdata.getMonth());
+console.log(newdata.getDay());
+
+// `${newdata.getDate()}`

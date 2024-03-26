143efb6f59d9c0cd50c06a85ec1706772d0a8751
c00ldy <mevadadhruval7529@gmail.com>
2024-03-26 23:20:48 +0530

    second basics

---
+++ 02basics/01_array.js
@@ -0,0 +1,20 @@
+let myarray = [0,1,2,3,4,5,]
+// console.log(myarray[1]);
+
+let newarr = ["thor" , "hulk"]
+// console.log(newarr[1]);
+
+// myarray.push(6);
+// myarray.push(7);
+// myarray.pop();
+
+myarray.shift();
+// myarray.unshift(7);
+// console.log(typeof myarray);
+// console.log(myarray.includes(9));
+// console.log(myarray.indexOf(9));
+
+// let a = myarray.join();
+// console.log(typeof a );
+
+// slice and slpice 

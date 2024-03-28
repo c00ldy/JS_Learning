b0217e4d961847cd41335ea8afabfec68e42f29b
c00ldy <mevadadhruval7529@gmail.com>
2024-03-28 17:56:00 +0530

    object again

---
+++ 02basics/05_object.js
@@ -0,0 +1,12 @@
+let course = {
+    sourcename : "chai or javascript",
+    price : "1000",
+    name : "dhruval",
+}
+
+
+// "{}" curly braces mean de constructing "
+
+let {sourcename : a} = course
+
+console.log(a);

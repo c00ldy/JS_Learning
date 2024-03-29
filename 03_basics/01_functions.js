029ff4bf23345b560d85610e5dca804abfd29fa7
c00ldy <mevadadhruval7529@gmail.com>
2024-03-29 15:09:52 +0530

    modified

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,7 +7,7 @@ function myfirstname (){
     console.log("a");
     console.log("l");
 }
-// myfirstname()
+myfirstname()
 
 function addition (num1 , num2) {
     // console.log(num1+num2);
@@ -16,10 +16,10 @@ function addition (num1 , num2) {
 }
 
 let b = addition(3,5)
-// console.log("result : ",b);
+console.log("result : ",b);
 
-// addition(3,null)
-// console.log(typeof addition)
+addition(3,null)
+console.log(typeof addition)
 
 function loginusermessage(username){
     if(username === undefined){
@@ -29,6 +29,6 @@ function loginusermessage(username){
 return`${username} just logged in`
 }
 
-// let d = loginusermessage("dhruval")
-// console.log(d);
+let d = loginusermessage("dhruval")
+console.log(d);
 console.log(loginusermessage());

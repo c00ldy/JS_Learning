ac05355f358c96cee6df0e0123411c540c0e04f7
c00ldy <mevadadhruval7529@gmail.com>
2024-03-29 19:05:21 +0530

    hoisting

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -1,11 +1,45 @@
 let a = 300;
-if(true){
-    let a = 30 
-    var b = 50 
-    const c = 70
-    console.log("inner:", a);
+if (true) {
+  let a = 30;
+  var b = 50;
+  const c = 70;
+  // console.log("inner:", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// function one() {
+//   const username = "dhrugval";
+
+//   function two() {
+//     const youtube = "coldy";
+//     console.log(username);
+//   }
+// //   console.log(youtube);
+//   two();
+// }
+
+// one()
+
+
+if (true) {
+    const website = "dhruval";
+    if (website === "dhruval") {
+        const name = "coldy";
+        // console.log(website + name);
+    }
+    // console.log(name);
+}
+// console.log(website);
+
+console.log(add(5))
+function add(num1){
+return num1 + 1
+}
+
+const bc = function newadd(num2) {
+    return num2 + 1
+}
+console.log(newadd(5))

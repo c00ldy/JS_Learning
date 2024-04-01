82a95ef84326cf530974e0e3c2604bd7fe0bb4e4
c00ldy <mevadadhruval7529@gmail.com>
2024-04-01 15:20:51 +0530

    switch case

--- 04_controllflow/switch.js
+++ 04_controllflow/switch.js
@@ -1,1 +1,29 @@
-g
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+  case 1:
+    console.log("january");
+    break;
+  case 2:
+    console.log("fab");
+    break;
+  case 3:
+    console.log("mar");
+    break;
+  case 4:
+    console.log("april");
+    break;
+
+  default:
+    console.log("nothing");
+    break;
+}

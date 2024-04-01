a7f861b801cae9d92e7d6650ca0c42fea8749aa9
c00ldy <mevadadhruval7529@gmail.com>
2024-04-01 16:54:58 +0530

    temp code and truthly value

---
+++ 04_controllflow/truthuly.js
@@ -0,0 +1,39 @@
+// const email = ""
+
+// if (email) {
+//     console.log("got email");
+// } else {
+//     console.log("dont get it");
+// }
+
+// falsy value : false , 0 , -0 , BigInt 0n ,"" , NaN , null , undifine 
+
+//truthy value : true , "0" , `false` , "  " , [ ] , {} , funcion (){}
+
+// if (email.length === 0 ) {
+//     console.log("is array");
+// }
+
+// const emptyboject ={}
+
+// if ((Object.keys(emptyboject)).length === 0 ) {
+//     console.log("is empty ");
+// }
+
+
+//nullish coalescing operator (??): null , undifined
+
+// let a;
+
+// a = 5 ?? 10;
+// a = null ?? 10;
+// a = undefined ?? 15;
+// a = null ?? 10 ?? 15;
+
+// console.log(a);
+
+//terniary opearator 
+//condition ? true : false
+
+let b = 900;
+b<=100 ? console.log("is chipper ") : console.log("is costly")

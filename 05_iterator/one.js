b350c0e601bba4170e66e2b5c65f8965a4466e79
c00ldy <mevadadhruval7529@gmail.com>
2024-04-01 23:04:51 +0530

    started loop

---
+++ 05_iterator/one.js
@@ -0,0 +1,48 @@
+//for loop 
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is the best number");
+//     }
+//     console.log(element);
+// }
+
+// loop inside loop 
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(` outer number is : ${i}`); 
+//     for (let j = 0; j <=10; j++) {
+//         // console.log(`inner number is: ${j} and outer number is ${i}`);  
+//         console.log(i + `*` + j + `=` + i*j);
+//     }
+// }
+
+// let myarray = [ "flash" , "hero" , "shiva"]
+// console.log(myarray.length);
+
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index];
+//     console.log(element);
+// }
+
+
+//break implement 
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`${i} ids the best`)
+//         break
+//     }
+//     console.log(i);
+// }
+
+//continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log(`${i} ids the best`)
+//         continue
+//     }
+//     console.log(i);
+// }

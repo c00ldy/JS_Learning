3e50430aef9121882434ab32a8db1e6a82c432d3
c00ldy <mevadadhruval7529@gmail.com>
2024-04-02 15:12:27 +0530

    while loops

---
+++ 05_iterator/two.js
@@ -0,0 +1,19 @@
+// let a = 0;
+
+// while (a <= 10) {
+//     console.log(`value of index is ${a}`);
+//     a = a+2
+// }
+
+// let myarray = [ "dhruval" , "maythadar" , "may"]
+// let arr = 0;
+// while (arr <= myarray.length) {
+//     console.log(`value is ${myarray[arr]}`);
+//     arr = arr +1 
+// }
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);

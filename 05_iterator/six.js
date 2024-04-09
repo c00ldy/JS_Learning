5ba4177c91ff038832488f5220fd4119c3bc6bf5
c00ldy <mevadadhruval7529@gmail.com>
2024-04-09 14:27:11 +0530

    after long time

---
+++ 05_iterator/six.js
@@ -0,0 +1,33 @@
+// const coding = ["py", "css" , "html" , "cpp"]
+
+// const value = coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// console.log(value);
+
+// const number = [ 1,5,8,4,6,98,7,8]
+// const val = number.filter((num) => {
+//     return num>5
+// }
+// )
+
+// console.log(val);
+
+const books = [
+  { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+  { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+  { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+  { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+];
+
+// const userbook = books.filter( (bk) => bk.edition === 2010)
+// console.log(userbook);
+
+const userbook = books.filter( (bk) => bk.edition > 2004)
+console.log(userbook);

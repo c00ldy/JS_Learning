df395b5a0c42ade92a7f96a1c3912b24900ba8cf
c00ldy <mevadadhruval7529@gmail.com>
2024-04-26 09:18:44 +0530

    ca

---
+++ 09_adavance_one/tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+new Promise(function(resolve,reject){
+// setTimeout(function(){
+//     console.log("asyink task 2");
+//     resolve()
+// },1000)
+// }).then(function() {
+//     console.log("asynk 3");
+// })
+
+// const promis = new Promise(function (resolve,reject) {
+//     setTimeout(function(){
+//         console.log("dhruval");
+//         resolve({username : "dhruval" , email : "dhruval@mail.co"})
+//     },1000)
+// })
+
+// promis.then(function(user){
+// console.log(user);
+// })
